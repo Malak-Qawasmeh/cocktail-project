dd9e5fab95e07c4a264b7077f1efd4c322087dad
ruaidris <ruaidris000@gmail.com>
2024-05-21 22:21:38 +0300

    Update Blender.java

--- src/cocktaill/Blender.java
+++ src/cocktaill/Blender.java
@@ -1,67 +1,66 @@
+import java.util.ArrayList;
 
-package cocktaill;
-
-
-class Blender {
-  //  Ingredient ArrayList<Ingredient>;
+public class Blender {
+    private ArrayList<Ingredient> ingredients;
     private int capacity;
     private int calories;
-    private int volume;
     private Color color;
+    private int volume;
 
-    public Blender(int capacity, int calories, int volume, Color color) {
+    public Blender(int capacity) {
         this.capacity = capacity;
-        this.calories = calories;
-        this.volume = volume;
-        this.color = color;
+        this.ingredients = new ArrayList<>();
+        this.calories = 0;
+        this.volume = 0;
+        this.color = new Color(0, 0, 0); // Default color
     }
 
-    public int getCapacity() {
-        return capacity;
+    public void add(Ingredient ingredient, Logger logger) {
+        if (this.volume + ingredient.getVolume() > this.capacity) {
+            logger.log("Adding ingredient failed: Blender overflow");
+            throw new BlenderOverflowException();
+        }
+        this.ingredients.add(ingredient);
+        this.volume += ingredient.getVolume();
+        this.calories += ingredient.getCalories();
+        this.color = blendColors(this.color, ingredient.getColor());
+        logger.log("Added ingredient: " + ingredient.getInfo());
     }
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
+    public Cocktail blend() throws BlenderOverflowException {
+        if (this.ingredients.isEmpty()) {
+            throw new BlenderEmptyException();
+        }
+        Cocktail cocktail = new Cocktail(this.calories, this.ingredients, this.color);
+        this.ingredients.clear();
+        this.volume = 0;
+        this.calories = 0;
+        this.color = new Color(0, 0, 0); // Reset to default color
+        return cocktail;
     }
 
-    public int getCalories() {
-        return calories;
+    public void pour(Cup cup) throws BlenderEmptyException {
+        if (this.ingredients.isEmpty()) {
+            throw new BlenderEmptyException();
+        }
+        if (cup.getCapacity() < this.volume) {
+            throw new BlenderOverflowException();
+        }
+        cup.setCalories(this.calories);
+        this.ingredients.clear();
+        this.volume = 0;
+        this.calories = 0;
+        this.color = new Color(0, 0, 0); // Reset to default color
     }
-
-    public void setCalories(int calories) {
-        this.calories = calories;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-    
 
     public String getInfo() {
-        return "volume = " + volume + ", capacity = " + capacity;
+        return "Blender[capacity=" + capacity + ", calories=" + calories + ", volume=" + volume + ", color=" + color + "]";
     }
 
- public void addIngredient(int volume, int ingredientVolume) throws BlenderOverloadException {
-        if (volume + ingredientVolume > capacity) {
-            throw new BlenderOverloadException("Blender capacity exceeded");
-        }
-        int name;
-        name = volume + ingredientVolume;
+    private Color blendColors(Color c1, Color c2) {
+        int r = (c1.getR() + c2.getR()) / 2;
+        int g = (c1.getG() + c2.getG()) / 2;
+        int b = (c1.getB() + c2.getB()) / 2;
+        return new Color(r, g, b);
     }
 }
-
-    
-    
-    
